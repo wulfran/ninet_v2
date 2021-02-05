41fcca9dba13fa7ffbfb02eb3ccd5b0e4ed1fd76
Piotr <piotr.nikonowicz@gmail.com>
2021-02-05 13:07:29 +0100

    Basic Config
    Hardcoded for now

--- app/Models/Config.php
+++ app/Models/Config.php
@@ -2,15 +2,40 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
-
-class Config extends Model
+/**
+ * Class Config for app configuration.
+ * TODO: Configs are hardcoded for now, after Invoice branch is done, rework this to DB store
+ * @package App\Models
+ */
+class Config
 {
-    protected $table = 'configs';
+    private $name, $tax_number, $address;
 
-    protected $fillable = [
-        'name', 'description', 'value', 'group'
-    ];
+    public function __construct()
+    {
+        $configs = config('configs');
 
-    public $timestamps = false;
+        foreach ($configs as $name => $value) {
+            $this->{$name} = $value;
+        }
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getTaxNumber()
+    {
+        return $this->tax_number;
+    }
+
+    public function getAddress()
+    {
+        $country = Country::find($this->address['country_id']);
+        unset($this->address['country_id']);
+        $this->address['country'] = $country->name;
+
+        return $this->address;
+    }
 }
